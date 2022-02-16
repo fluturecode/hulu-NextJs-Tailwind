78faa73673531b2b284f29238bd31f2c96bb7eb9
fluturecode <fluturecode@gmail.com>
2022-02-16 18:10:15 -0500

    added mobile styles to Header.js

--- components/Header.js
+++ components/Header.js
@@ -1,13 +1,18 @@
 import Image from 'next/image'
 import logo from '../assets/logo.png';
 import { HeaderItem } from './HeaderItem';
-import { HomeIcon } from '@heroicons/react/outline';
+import { BadgeCheckIcon, CollectionIcon, HomeIcon, LightningBoltIcon, SearchIcon, UserIcon } from '@heroicons/react/outline';
 
 export const Header = () => {
   return (
-    <header className=''>
-        <div>
+    <header className='flex flex-col sm:flex-row m-5 justify-between items-center h-auto'>
+        <div className='flex flex-grow justify-evenly max-w-2xl'>
           <HeaderItem title='HOME' Icon={HomeIcon}/>
+          <HeaderItem title='TRENDING' Icon={LightningBoltIcon}/>
+          <HeaderItem title='VERIFIED' Icon={BadgeCheckIcon}/>
+          <HeaderItem title='COLLECTIONS' Icon={CollectionIcon}/>
+          <HeaderItem title='SEARCH' Icon={SearchIcon}/>
+          <HeaderItem title='ACCOUNT' Icon={UserIcon}/>
         </div>
       <Image 
         className='object-contain'
