9ffb7b8aa5038bd9ccc32131eab60e52af48e349
fluturecode <fluturecode@gmail.com>
2022-02-16 19:16:46 -0500

    added styled to Nav.js

--- components/Nav.js
+++ components/Nav.js
@@ -1,7 +1,15 @@
+import requests from '../utils/requests';
+
 export const Nav = () => {
   return (
     <nav>
-      <div></div>
+      <div className=''>
+        {Object.entries(requests).map(([key, {title, url}]) => (
+          <h2 key={key} className='cursor-pointer transition duration-100 transform hover:scale-125 hover:text-white active:text-red-500'>
+            {title}
+          </h2>
+        ))}
+      </div>
     </nav>
   )
 }
