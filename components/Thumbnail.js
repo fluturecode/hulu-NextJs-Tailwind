7106131844b568592e2476c12034fce7d5be772f
fluturecode <fluturecode@gmail.com>
2022-02-25 14:22:41 -0500

    fixed styling in Thumbnail.js

--- components/Thumbnail.js
+++ components/Thumbnail.js
@@ -5,7 +5,7 @@ export const Thumbnail = ({ result }) => {
   const BASE_URL = 'https://image.tmdb.org/t/p/original/';
 
   return (
-    <div className='group'>
+    <div className='p-2 group cursor-pointer transition duratinon-200 ease-in transform sm:hover:scale-105 hover:z-50'>
       <Image
         layout='responsive'
         src={
@@ -17,7 +17,7 @@ export const Thumbnail = ({ result }) => {
       />
       <div className='p-2'>
         <p className='truncate max-w-md'>{result.overview}</p>
-        <h2>{result.title || result.original_name}</h2>
+        <h2 className='mt-1 text-2xl text-white transition-all duration-100 ease-in-out group-hover:font-bold'>{result.title || result.original_name}</h2>
         <p className='flex items-center opacity-0 group-hover:opacity-100'>
           {result.media_type && `${result.media_type} •`}{' '}
           {result.release_date || result.first_air_date} •{' '}
