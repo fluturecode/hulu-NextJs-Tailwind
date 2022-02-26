7eee2be5e0eab2d408fac57a6910bb80e48e52f9
fluturecode <fluturecode@gmail.com>
2022-02-25 19:57:22 -0500

    added custom tailwind class to config

--- components/Results.js
+++ components/Results.js
@@ -3,7 +3,7 @@ import { Thumbnail } from './Thumbnail';
 
 export const Results = ({ results }) => {
   return (
-    <div className='px-5 my-10 sm:grid md:grid-cols-2 xl:grid-cols-3 3xl:flex flex-wrap justify-center'>
+    <div className='px-5 my-8 sm:grid md:grid-cols-2 xl:grid-cols-3 3xl:flex flex-wrap justify-center'>
       {results.map(result => (
         <Thumbnail key={result.id} result={result} />
       ))}
--- tailwind.config.js
+++ tailwind.config.js
@@ -5,7 +5,11 @@ module.exports = {
     './index.html',
   ],
   theme: {
-    extend: {},
+    extend: {
+      screens: {
+        '3xl': '2000px',
+      },
+    },
   },
   variants: {
     extend: {},
